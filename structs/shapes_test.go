783718fe6d22915f8336ca046713481343a7d09d
Connor Keogh <contact@kyoh.run>
2023-12-20 11:06:32 +1100

    Add structs package

---
+++ structs/shapes_test.go
@@ -0,0 +1,34 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rect := Rectangle{10.0, 10.0}
+	got := Perimeter(rect)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	t.Run("rectangles", func(t *testing.T) {
+		rect := Rectangle{5.0, 10.0}
+		got := rect.Area()
+		want := 50.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+}
